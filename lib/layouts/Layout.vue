0f7908d975392439323cc5c51899ec3041ed8c5a
xuxiaoyan <xuxiaoyan@didiglobal.com>
2021-07-19 20:24:57 +0800

    add theme core files

---
+++ lib/layouts/Layout.vue
@@ -0,0 +1,39 @@
+<template>
+  <Layout>
+    <template #navbar-before>
+      <slot name="navbar-before" />
+    </template>
+    <template #navbar-after>
+      <slot name="navbar-after" />
+    </template>
+    <template #sidebar-top>
+      <slot name="sidebar-top" />
+    </template>
+    <template #sidebar-bottom>
+      <slot name="sidebar-bottom" />
+    </template>
+    <template #page-top>
+      <slot name="page-top" />
+    </template>
+    <template #page-bottom>
+      <slot name="page-bottom" />
+    </template>
+  </Layout>
+</template>
+
+<script>
+import Layout from '@vuepress/theme-default/lib/client/layouts/Layout.vue'
+import '../styles/index.styl'
+
+export default {
+  components: {
+    Layout,
+  }
+}
+</script>
+
+<style lang="css">
+.my-footer {
+  text-align: center;
+}
+</style>
