5ceebb347f84ce2845b5b61088fc65d8d9814dfa
xuxiaoyan <xuxiaoyan@kuaishou.com>
2021-11-25 19:31:11 +0800

    add slots

--- lib/layouts/Layout.vue
+++ lib/layouts/Layout.vue
@@ -3,17 +3,26 @@
     <template #navbar-before>
       <slot name="navbar-before" />
     </template>
+    <template #navbar>
+      <slot name="navbar" />
+    </template>
     <template #navbar-after>
       <slot name="navbar-after" />
     </template>
     <template #sidebar-top>
       <slot name="sidebar-top" />
     </template>
+    <template #sidebar>
+      <slot name="sidebar" />
+    </template>
     <template #sidebar-bottom>
       <slot name="sidebar-bottom" />
     </template>
     <template #page-top>
       <slot name="page-top" />
+    </template>
+    <template #page>
+      <slot name="page" />
     </template>
     <template #page-bottom>
       <slot name="page-bottom" />
